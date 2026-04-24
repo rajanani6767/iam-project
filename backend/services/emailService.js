daa733df167fde7310e38f7c3666ec7e97146218
Raja <shaikraja712@gmail.com>
2026-04-24 11:33:53 +0530

    email

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,14 +1,8 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const sendOtpEmail = async (to, otp) => {
-  return await resend.emails.send({
-    from: "noreply@send.loginauth.site" // ✅ your domain
-    to: to, // ✅ correct
-    subject: "Your OTP Code 🔐",
-    html: `<h2>Your OTP is: ${otp}</h2>`,
+  return resend.emails.send({
+    from: "noreply@send.loginauth.site",
+    to: to,
+    subject: "OTP",
+    html: `<h2>${otp}</h2>`
   });
 };
-
-module.exports = { sendOtpEmail };
