f34c109f360381194e1a86556c9426f8297bed07
Raja <shaikraja712@gmail.com>
2026-04-25 21:54:03 +0530

    fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -44,11 +44,15 @@ router.post("/logout", (req, res) => {
 router.post(
   "/register",
   [
-    body("username").isEmail().withMessage("Enter valid email ❌"),
-    body("password")
-      .isLength({ min: 8 })
-      .withMessage("Password must be at least 8 characters ❌"),
-  ],
+  body("username").isEmail().withMessage("Enter valid email ❌"),
+  body("password")
+    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters ❌")
+    .matches(/[a-z]/).withMessage("Password must contain a lowercase letter ❌")
+    .matches(/[A-Z]/).withMessage("Password must contain an uppercase letter ❌")
+    .matches(/[0-9]/).withMessage("Password must contain a number ❌")
+    .matches(/[@$!%*?&]/).withMessage("Password must contain a special character ❌"),
+],
+
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
